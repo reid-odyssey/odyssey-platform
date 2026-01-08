04ca08b4bfc64174a662af73d875f0151b6224c7
reid-odyssey <reid@odyssey.stream>
2026-01-08 14:09:36 +1300

    chore: relax eslint rules for production build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,12 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@next/next/no-img-element": "off",
+      "@next/next/no-html-link-for-pages": "off"
+    },
     ignores: [
       "node_modules/**",
       ".next/**",
